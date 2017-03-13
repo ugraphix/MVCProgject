009f113d1ba9e4d61e206232cb3f9943c4df04e2
Gracie <grace@ugraphix.com>
2017-03-13 10:13:04 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAssignment7.Startup))]
+namespace MVCAssignment7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
