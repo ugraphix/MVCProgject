a462af112ac1cf297011d5239e0112dc966e18a5
Gracie <grace@ugraphix.com>
2017-03-13 10:35:47 -0700

    Add files via upload

---
+++ Models/PersonLite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment7.Models
+{
+    public class PersonLite
+    {
+        public int PersonKey { set; get; }
+        public string LastName { set; get; }
+        public string FirstName { set; get; }
+        public string Email { set; get; }
+        public string Apartment { set; get; }
+        public string Street { set; get; }
+        public string State { set; get; }
+        public string Zipcode { set; get; }
+        public string HomePhone { set; get; }
+        public string WorkPhone { set; get; }
+        public string Password { set; get; }
+
+    }
+}
